af744598410442ea1d3d21a941f27547b631a238
vikram-devil <109408021+vikram-devil@users.noreply.github.com>
2022-07-16 12:12:51 +0530

    Add files via upload

---
+++ add 2 array.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+vector<int> add(vector<int> arr1,vector<int> arr2){
+	int i=arr1.size()-1;
+	int j=arr2.size()-1;
+	vector<int> ans;
+	
+	while(i>=0 || j>=0){
+		int k=arr1[i]+arr2[j];
+		if(k>9 and i-1>=0){
+			ans.push_back(k%10);
+			arr1[i-1]+=1;
+		}
+		else if(k>9 and i==0){
+		    ans.push_back(k);
+		}
+		else{
+			ans.push_back(k);
+		}
+		i--;
+		j--;
+	}
+	
+	while(i>=0){
+		//cout<<arr1[i];
+		ans.push_back(arr1[i]);
+		i--;
+	}
+	return ans;
+	
+}
+
+int main(){
+	
+	vector<int> arr1={1,8,9};
+	vector<int> arr2={9,7,6};
+	
+	vector<int> ans=add(arr1,arr2);
+	reverse(ans.begin(),ans.end());
+	for(auto i:ans){
+		cout<<i;
+	}
+	return 0;
+}
